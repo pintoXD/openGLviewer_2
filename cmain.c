14dffc3711fd7aae276412283348a9a101747119
johnpnx <joaopedro1172@gmail.com>
2019-10-22 16:57:23 -0300

    prestaging the trasintion to C

--- cmain.c
+++ cmain.c
@@ -13,11 +13,14 @@
 #ifdef __linux__
     #include <GL/glut.h>         
 #endif
+
+#include <stdio.h>
+#include <errno.h>
  
 void displayMe(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_POINTS);
+    glBegin(GL_POLYGON);
        
         glVertex3f(0.5, 0.0, 0.5);
         glVertex3f(0.5, 0.0, 0.0);
@@ -26,20 +29,65 @@ void displayMe(void)
     glEnd();
     //glPointSize(10.0);  // doesn't work as I expected
     glBegin(GL_POINTS);
-       
         glVertex3f(0.5, 0.0, 0.5);
     glEnd();
     glFlush();
 }
- 
+
+int open_cloud_file()
+{
+    //open the cloud file
+}
+
+void read_cloud_line()
+{
+    // operatins read each cloud line
+}
+
+void draw_cloud()
+{
+    // use openGl vertex to draw 
+}
+
+void defualtBody(int argc, char** argv)
+{
+        glutInit(&argc, argv);
+        glutInitDisplayMode(GLUT_SINGLE);
+        glutInitWindowSize(800, 600);
+        glutInitWindowPosition(100, 100);
+        glutCreateWindow("Hello world!");
+        glutDisplayFunc(displayMe);
+        glutMainLoop();
+
+        /* 
+        glutInit(&argc, argv);
+        glutInitDisplayMode(GLUT_SINGLE);
+        glutInitWindowSize(800, 600);
+        glutInitWindowPosition(100, 100);
+        glutCreateWindow("Hello world!");
+        glutDisplayFunc(displayMe);
+        glutMainLoop(); 
+        */
+        
+}
+
+
+
 int main(int argc, char** argv)
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE);
-    glutInitWindowSize(800, 600);
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow("Hello world!");
-    glutDisplayFunc(displayMe);
-    glutMainLoop();
-    return 0;
+        //defualtBody(argc, argv);
+        
+
+        FILE *cloudFile;
+        cloudFile = fopen ("./1cloud.xyz","r");
+
+        if (cloudFile == NULL) {
+                printf ("File not created okay, errno = %d\n", errno);
+                return 1;
+        }
+        fclose (cloudFile);
+        printf ("File opened okay\n");
+        return 0;
+        
+        return 0;
 }
